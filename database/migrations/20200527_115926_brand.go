15ff54181a7005181b84f4ca9e2ad87f8e8a5000
Autsakorn Tanyianniti <autsakorn@Autsakorns-MacBook-Pro-2.local>
2020-05-27 20:21:45 +0700

    Add delete migration brand row

--- database/migrations/20200527_115926_brand.go
+++ database/migrations/20200527_115926_brand.go
@@ -17,14 +17,15 @@ func init() {
 	migration.Register("Brand_20200527_115926", m)
 }
 
-// Run the migrations
+// Up Run the migrations
 func (m *Brand_20200527_115926) Up() {
 	// use m.SQL("CREATE TABLE ...") to make schema update
-	m.SQL("CREATE TABLE brand(id serial primary key,title char(255) NOT NULL,slug TEXT NOT NULL,created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL)")
+	m.SQL("CREATE TABLE brand(id serial primary key,title char(255) NOT NULL,slug TEXT NOT NULL,created_at TIMESTAMP WITH TIME ZONE NOT NULL,updated_at TIMESTAMP WITH TIME ZONE NOT NULL)")
 }
 
-// Reverse the migrations
+// Down Reverse the migrations
 func (m *Brand_20200527_115926) Down() {
 	// use m.SQL("DROP TABLE ...") to reverse schema update
 	m.SQL("DROP TABLE brand")
+	m.SQL("DELETE FROM migrations WHERE name = 'Brand_20200527_115926'")
 }
